b61434a084ff24bf41372d813778b2243d2fefe6
romann-broque <kinoide56@gmail.com>
2023-11-19 12:23:50 +0100

    start MVE1-SINGLE_CONNECTION

--- dependencies/Data.hpp
+++ dependencies/Data.hpp
@@ -1,18 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.hpp                                         :+:      :+:    :+:   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 00:49:22 by rbroque           #+#    #+#             */
-/*   Updated: 2023/11/19 12:16:00 by rbroque          ###   ########.fr       */
+/*   Created: 2023/11/19 12:15:38 by rbroque           #+#    #+#             */
+/*   Updated: 2023/11/19 12:15:56 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "irc.hpp"
-#include <sys/socket.h>
 
-int	setUpServer(Data *const obj);
+class Data
+{
+	public :
+		int					sockfd;
+		int 				servfd;
+		struct sockaddr_in	address;
+		socklen_t			addrlen;
+};
